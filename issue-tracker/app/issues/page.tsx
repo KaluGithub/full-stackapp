90c3182fbc41e0168872ccf666cc9989a9af33ec
Mona Kalu <ukpaimona@gmail.com>
2024-03-19 07:43:13 -0500

    Set Up Radix Ui

--- issue-tracker/app/issues/page.tsx
+++ issue-tracker/app/issues/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const issuesPage = () => {
-  return <div>issuesPage</div>;
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
 };
 
 export default issuesPage;
